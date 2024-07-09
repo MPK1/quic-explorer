f1f08512a8c7801ff4fabb8224aa1ddcf877a8bf
Marcel Kempf <kempfm@net.in.tum.de>
2024-07-09 12:02:48 +0200

    add missing composable

---
+++ web/composables/useConfirm.ts
@@ -0,0 +1,29 @@
+interface ConfirmParams {
+  title: string;
+  message: string;
+  label: string;
+  action: Function;
+}
+
+const confirming = ref<boolean>(false);
+
+const params = ref<ConfirmParams>({
+  title: "Title",
+  message: "Description",
+  label: "Confirm",
+  action: () => {},
+});
+
+export default function () {
+  function confirm(
+    title: string,
+    message: string,
+    label: string,
+    action: Function,
+  ) {
+    params.value = { title, message, label, action };
+    confirming.value = true;
+  }
+
+  return { confirm, confirming, params };
+}
